1fb121ebf512c0835bd692546fa180ffe1a7bfee
Leo <huangshan.glx@gmail.com>
2021-04-21 20:09:33 +0800

    状态模式

---
+++ src/chapter10/gumballstate/State.java
@@ -0,0 +1,13 @@
+package chapter10.gumballstate;
+
+public interface State {
+    void insertQuarter();
+
+    void ejectQuarter();
+
+    void turnCrank();
+
+    void dispense();
+
+    void refill();
+}
